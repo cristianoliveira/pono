b19dbb46ba012bde37fb11a3ac0539495f7f5e7d
cristianoliveira <contato@cristianoliveira.com.br>
2024-09-13 15:03:05 +0200

    chore: adds dev dependencies

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "slot - Symbolic link organizer tool";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    utils.url = "github:numtide/flake-utils";
+    copkgs.url = "github:cristianoliveira/nixpkgs";
+  };
+  outputs = { self, nixpkgs, utils, copkgs, ... }: 
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        cpkgs = import copkgs { inherit pkgs; };
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [ 
+            pkgs.cargo
+            cpkgs.funzzy
+          ];
+        };
+    });
+}
