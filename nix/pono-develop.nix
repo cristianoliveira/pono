84a6d1699d204badb8092e512080fd7252664d16
cristianoliveira <contato@cristianoliveira.com.br>
2024-10-14 02:57:12 +0200

    chore(nix): bump hases for nightly
    (cherry picked from commit e5d35dcb2385f509e77d79a2184d1f35795971a4)

--- nix/pono-develop.nix
+++ nix/pono-develop.nix
@@ -1,16 +1,17 @@
 { lib , rustPlatform , fetchFromGitHub , stdenv , darwin }:
 rustPlatform.buildRustPackage rec {
   pname = "pono";
-  version = "main";
+  ## Latest commit on main branch
+  version = "efa714c7e44933b0904a1b6001745e1b664715cc";
 
   src = fetchFromGitHub {
     owner = "cristianoliveira";
     repo = "pono";
     rev = "${version}";
-    hash = "sha256-5PyiJ/n9n3CvDMOd7OVv1KK9uwWLLTnx9TfDzEDboX4=";
+    hash = "sha256-LrtGvXTiTOl7gJzfqzOkmZe8Nf7e71e65GW+P4jD/FM=";
   };
 
-  cargoHash = "sha256-RhrzKeLn5R68zQAYteGQF4hg4eqBNbF+Z9du39dZSfc=";
+  cargoHash = "sha256-PkBDKuY5l9yHeGbA55l9rsspjBFx4zssIWxy7QAqDsg=";
 
   buildInputs = lib.optionals stdenv.isDarwin [
     darwin.apple_sdk.frameworks.CoreServices
