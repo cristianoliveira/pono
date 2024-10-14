c441959872ef00670008ca0d240e9a36b3f9d54f
cristianoliveira <contato@cristianoliveira.com.br>
2024-10-14 02:18:01 +0200

    chore(nix): bump nix nightly pkg

--- nix/pono-develop.nix
+++ nix/pono-develop.nix
@@ -1,16 +1,16 @@
 { lib , rustPlatform , fetchFromGitHub , stdenv , darwin }:
 rustPlatform.buildRustPackage rec {
   pname = "pono";
-  version = "develop";
+  version = "master";
 
   src = fetchFromGitHub {
     owner = "cristianoliveira";
     repo = "pono";
     rev = "${version}";
-    hash = "sha256-PYRJwpi+RCEUixDN7/FP0WQdlaFa67QHn3u4qRahLq4=";
+    hash = "sha256-OQVNVO1ZEHPtm0Ofcg41w5JyjBo1oCSYUMfmHYLFTqs=";
   };
 
-  cargoHash = "sha256-BQ7TYVtU87fGPoLHQx2Ouc97BiGT4KyzKzlvNE9e2Jg=";
+  cargoHash = "sha256-HolBfmAo/1HzLmVU7a8raC7X5ZXmqhZn7YE2CLnL1ns=";
 
   buildInputs = lib.optionals stdenv.isDarwin [
     darwin.apple_sdk.frameworks.CoreServices
